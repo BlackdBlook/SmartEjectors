c36d6d15088506d30a2af889628cb4f439870162
Daniel He <me@daniel-egg.com>
2022-01-25 14:40:40 -0800

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+
+namespace SmartEjectors
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string GUID = "com.daniel-egg.smartejectors";
+        private const string NAME = "Smart Ejectors";
+        private const string VERSION = "1.0.0";
+        private void Awake()
+        {
+
+        }
+    }
+}
